c587e46c6ca152773078a5c8ffed78facdec63ec
Epicanaconda1 <46452519+Epicanaconda1@users.noreply.github.com>
2025-11-23 20:49:00 +0100

    Uncomment imports and includes in tb_env_pkg.sv

--- top/tb/tb_env_pkg.sv
+++ top/tb/tb_env_pkg.sv
@@ -5,27 +5,27 @@ package tb_env_pkg;
 
    import probe_pkg::*;
    import tb_top_core_odi_oled_ctrl_bind::*;   
-//   import tb_top_core_dti_dti_spi_bind::*;   
-//   import tb_top_core_dti_bind::*;   
+   import tb_top_core_dti_dti_spi_bind::*;   
+   import tb_top_core_dti_bind::*;   
    import tb_top_core_zu_aes128_bind::*;   
     
    import oled_spi_agent_pkg::*;
-//   import spi_4wire_agent_pkg::*;
+   import spi_4wire_agent_pkg::*;
    import kb_axi4stream_agent_pkg::*;
    
    import tb_env_base_pkg::*;
 
-//  `include "spi_4wire_seq.svh";
+   `include "spi_4wire_seq.svh";
 
   `include "scoreboard_oled_spi_base.sv"
   `include "scoreboard_oled_spi.sv"
   `include "coverage_oled_spi_base.svh"
   `include "coverage_oled_spi.svh"
 
-//  `include "scoreboard_dti_spi_base.sv"
-//  `include "scoreboard_dti_spi.sv"
-//  `include "coverage_dti_spi_base.svh"
-//  `include "coverage_dti_spi.svh"
+   `include "scoreboard_dti_spi_base.sv"
+   `include "scoreboard_dti_spi.sv"
+   `include "coverage_dti_spi_base.svh"
+   `include "coverage_dti_spi.svh"
 
   `include "scoreboard_zu_base.sv"
   `include "scoreboard_zu_aes128.sv"
@@ -34,4 +34,3 @@ package tb_env_pkg;
   `include "tb_env.svh";
    
 endpackage // tb_env_pkg
-   
