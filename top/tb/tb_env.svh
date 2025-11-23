3be60607da88b50c4bed4421248ead4ee531b18c
Epicanaconda1 <46452519+Epicanaconda1@users.noreply.github.com>
2025-11-23 20:47:56 +0100

    Uncomment DTI SPI agent and scoreboard connections

--- top/tb/tb_env.svh
+++ top/tb/tb_env.svh
@@ -13,8 +13,8 @@ class tb_env #(int AXI4_ADDRESS_WIDTH   = 32,
    typedef oled_spi_agent  oled_spi_agent_t;
    oled_spi_agent_t        m_oled_spi_agent;
 
-//   typedef spi_4wire_agent spi_4wire_agent_t;
-//   spi_4wire_agent_t       m_spi_4wire_agent;
+   typedef spi_4wire_agent spi_4wire_agent_t;
+   spi_4wire_agent_t       m_spi_4wire_agent;
    
    // Declare the DNIC agent interface
    typedef kb_axi4stream_agent kb_axi4stream_agent_t;
@@ -28,10 +28,10 @@ class tb_env #(int AXI4_ADDRESS_WIDTH   = 32,
    // coverage_oled_spi_base   m_coverage_oled_spi_base;
 
    // Declare DTI SPI scoreboard base class
-//   scoreboard_dti_spi_base m_scoreboard_dti_spi_base;
+    scoreboard_dti_spi_base m_scoreboard_dti_spi_base;
 
    // Declare DTI SPI coverage_base class 
-//   coverage_dti_spi_base    m_coverage_dti_spi_base;
+    coverage_dti_spi_base    m_coverage_dti_spi_base;
 
    // Declare ZU scoreboard base class
    scoreboard_zu_base       m_scoreboard_zu_base;
@@ -46,15 +46,15 @@ class tb_env #(int AXI4_ADDRESS_WIDTH   = 32,
       super.build_phase(phase);
 
       m_oled_spi_agent       = oled_spi_agent_t::type_id::create("m_oled_spi_agent",this);     
-      // m_spi_4wire_agent      = spi_4wire_agent_t::type_id::create("m_spi_4wire_agent",this);     
+      m_spi_4wire_agent      = spi_4wire_agent_t::type_id::create("m_spi_4wire_agent",this);     
       m_kb_axi4stream_agent  = kb_axi4stream_agent_t::type_id::create("m_kb_axi4stream_agent",this);     
 
       m_scoreboard_oled_spi_base  = scoreboard_oled_spi_base::type_id::create("m_scoreboard_oled_spi_base", this);
       // Removed to reduce complextity of OLED_SPI and SCU testcases
       // m_coverage_oled_spi_base    = coverage_oled_spi_base::type_id::create("m_coverage_oled_spi_base", this);
 
-      // m_scoreboard_dti_spi_base   = scoreboard_dti_spi_base::type_id::create("m_scoreboard_dti_spi_base", this);
-      // m_coverage_dti_spi_base     = coverage_dti_spi_base::type_id::create("m_coverage_dti_spi_base", this);
+      m_scoreboard_dti_spi_base   = scoreboard_dti_spi_base::type_id::create("m_scoreboard_dti_spi_base", this);
+      m_coverage_dti_spi_base     = coverage_dti_spi_base::type_id::create("m_coverage_dti_spi_base", this);
 
       m_scoreboard_zu_base   = scoreboard_zu_base::type_id::create("m_scoreboard_zu_base", this);
      
@@ -68,10 +68,9 @@ class tb_env #(int AXI4_ADDRESS_WIDTH   = 32,
       // Removed to reduce complextity of OLED_SPI and SCU testcases
       // m_oled_spi_agent.m_monitor.ap.connect(m_coverage_oled_spi_base.analysis_export);
 
-      // m_spi_4wire_agent.m_monitor.ap.connect(m_scoreboard_dti_spi_base.dti_spi_monitor_out.analysis_export);
-      // m_spi_4wire_agent.m_monitor.ap.connect(m_coverage_dti_spi_base.analysis_export);      
+      m_spi_4wire_agent.m_monitor.ap.connect(m_scoreboard_dti_spi_base.dti_spi_monitor_out.analysis_export);
+      m_spi_4wire_agent.m_monitor.ap.connect(m_coverage_dti_spi_base.analysis_export);      
       
    endfunction // connect_phase
    
 endclass
-
